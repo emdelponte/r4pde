66950750a85e2a0630beb81a4dfe7097aba9096d
Emerson M. Del Ponte <edelponte@gmail.com>
2023-06-06 18:51:04 -0300

    update

--- R/plot_DSAF.R
+++ R/plot_DSAF.R
@@ -1,23 +1,23 @@
 #' Plot Dynamics and Structure Analysif of Foci (plot_DSAF)
 #'
-#' This function creates a tile plot of the foci identified by the DSAF function.
-#' It colors each cell in a foci and labels the centroid of each cluster with the cluster ID.
+#' This function creates a tile plot of the foci (cluster) identified by the DSAF function.
+#' It colors each cell in a foci and labels the centroid of each cluster with the foci ID.
 #' The 'ggplot2' package is used for the plot, and will be automatically installed if not already present.
 #'
 #' @param df A dataframe containing at least three columns: 'x', 'y', and 'cluster_id'.
 #'           'x' and 'y' are spatial coordinates and 'cluster_id' is the cluster identifier
 #'           to which each cell belongs.
 #'
-#' @return A ggplot object with the scatter plot of clusters.
+#' @return A ggplot object with the scatter plot of foci (clusters).
 #'
 #' @examples
 #' df <- data.frame(x = sample(1:100, 500, replace = TRUE),
 #'                  y = sample(1:100, 500, replace = TRUE),
 #'                  i = sample(0:1, 500, replace = TRUE, prob = c(0.7, 0.3)))
 #'
-#' # Perform the disease spread analysis
+#' # Perform the DSAF
 #' result <- DSAF(df)
-#' # Plot the clusters
+#' # Plot the foci
 #' plot_DSAF(result[[3]])
 #'
 #' @importFrom ggplot2 ggplot geom_tile theme_minimal geom_text
